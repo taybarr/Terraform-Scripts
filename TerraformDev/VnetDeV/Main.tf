408c39af2864bd00c07a8c3982baf5c376216efb
taybarr <95315941+taybarr@users.noreply.github.com>
2022-04-14 08:27:54 -0700

    Add files via upload

---
+++ TerraformDev/VnetDeV/Main.tf
@@ -0,0 +1,47 @@
+########################################################
+#This is a developement script for creating user roles#
+########################################################
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=2.97.0"
+    }
+  }
+}
+provider "azurerm" { 
+    features{}
+    subscription_id = "c998da0a-2c4e-4e5f-9741-24a6aa81b169" 
+  }
+  resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
+
+
+}
