ab57ff2b53b07f7ad7c489ddde9051dd2d666d23
taybarr <95315941+taybarr@users.noreply.github.com>
2022-04-18 10:44:28 -0700

    Update Main.tf

--- TerraformDev/VnetDeV/Main.tf
+++ TerraformDev/VnetDeV/Main.tf
@@ -42,6 +42,9 @@ resource "azurerm_virtual_network" "example" {
     address_prefix = "10.0.2.0/24"
     security_group = azurerm_network_security_group.example.id
   }
+  /*   tags = {
+    Hello = "World"
+  } */
 
 
 }
